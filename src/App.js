82ea70cda7d4334584370e7e6c4ba60cb4836ee4
Vivek Nikam <nikam.9127@gmail.com>
2022-11-14 12:02:32 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [movie, setMovie] = useState([]);
 
   const changeText = (event) => {
-    // console.log(event)
+    console.log(event)
     setText(event.target.value);
   };
 
@@ -24,25 +24,77 @@ function App() {
 
   return (
     <>
-      <header className="head">
-      
+      <body>
+        <h1 style={{color:"#ffe98152"}}>
+        ~ MOVIE SEARCHING APP 
+        </h1>
+        <header className="head">
+          <form action="" onSubmit={getMovie}>
+            <input
+              className="inp"
+              type="search"
+              placeholder="Search Movie"
+              aria-label="Search"
+              onChange={changeText}
+            />
+            <button className="btn" type="submit">
+              Search
+            </button>
+          </form>
+        </header>
+      </body>
 
-        <form action=""onSubmit={getMovie}>
-        <input
-            className="inp"
-            type="search"
-            placeholder="Search Movie"
-            aria-label="Search"
-            onChange={changeText}
-          />
-          <button className="btn" type="submit" >
-            Search
-          </button>
-        </form>
-        
-       
-      </header>
-      {/* <nav id="nav" className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container my-3">
+        <div className="row">
+          {movie.map((value, index) => {
+            return (
+              <div className="col-3" key={index}>
+                <div className="card" style={{ width: "18rem" }}>
+                  <img src={value.Poster} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h3 className="card-title"># {value.Title}</h3>
+                    <h4 className="card-text">YEAR : {value.Year}</h4>
+                    <h5 className="card-text">TYPE : -{value.Type}-</h5>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /* <nav id="nav" className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
             Movie App
@@ -74,30 +126,5 @@ function App() {
             </form>
           </div>
         </div>
-      </nav> */}
-
-      <div className="container my-3">
-        <div className="row">
-          {movie.map((value, index) => {
-            return (
-              <div className="col-3" key={index}>
-                <div className="card" style={{ width: "18rem" }}>
-                  <img src={value.Poster} className="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <h3 className="card-title">
-                      # {value.Title}
-                    </h3>
-                    <h4 className="card-text">YEAR : {value.Year}</h4>
-                    <h5 className="card-text">TYPE : -{value.Type}-</h5>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </>
-  );
+      </nav> */
 }
-
-export default App;
