f893c0a915e5bf848f90242bc11e22c805c4baee
Vivek Nikam <102734695+GithubVicky97@users.noreply.github.com>
2022-11-14 12:07:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [movie, setMovie] = useState([]);
 
   const changeText = (event) => {
-    // console.log(event)
+    console.log(event)
     setText(event.target.value);
   };
 
@@ -24,57 +24,25 @@ function App() {
 
   return (
     <>
-      <header className="head">
-      
-
-        <form action=""onSubmit={getMovie}>
-        <input
-            className="inp"
-            type="search"
-            placeholder="Search Movie"
-            aria-label="Search"
-            onChange={changeText}
-          />
-          <button className="btn" type="submit" >
-            Search
-          </button>
-        </form>
-        
-       
-      </header>
-      {/* <nav id="nav" className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Movie App
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-           
-            <form className="d-flex" role="search" onSubmit={getMovie}>
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search Movie"
-                aria-label="Search"
-                onChange={changeText}
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search 
-              </button>
-            </form>
-          </div>
-        </div>
-      </nav> */}
+      <body>
+        <h1 style={{color:"#ffe98152"}}>
+        ~ MOVIE SEARCHING APP 
+        </h1>
+        <header className="head">
+          <form action="" onSubmit={getMovie}>
+            <input
+              className="inp"
+              type="search"
+              placeholder="Search Movie"
+              aria-label="Search"
+              onChange={changeText}
+            />
+            <button className="btn" type="submit">
+              Search
+            </button>
+          </form>
+        </header>
+      </body>
 
       <div className="container my-3">
         <div className="row">
@@ -84,9 +52,7 @@ function App() {
                 <div className="card" style={{ width: "18rem" }}>
                   <img src={value.Poster} className="card-img-top" alt="..." />
                   <div className="card-body">
-                    <h3 className="card-title">
-                      # {value.Title}
-                    </h3>
+                    <h3 className="card-title"># {value.Title}</h3>
                     <h4 className="card-text">YEAR : {value.Year}</h4>
                     <h5 className="card-text">TYPE : -{value.Type}-</h5>
                   </div>
@@ -101,3 +67,10 @@ function App() {
 }
 
 export default App;
+
+{
+
+
+
+
+
